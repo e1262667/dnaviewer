de721f2deb2e39565f8c6a0ef784fa1091a08a0f
e1262667@trbvm.com <e1262667@trbvm.com>
2015-03-02 12:08:20 -0500

    refactor

---
+++ client/tests/unit/utils/plasmid-map-test.js
@@ -0,0 +1,10 @@
+import plasmidMap from '../../../utils/plasmid-map';
+import { module, test } from 'qunit';
+
+module('plasmidMap');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = plasmidMap();
+  assert.ok(result);
+});
